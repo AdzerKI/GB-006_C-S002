a5f52d556ef7f7fbd00adf2cc192e51498a84eea
AdzerKI <silver_cat@list.ru>
2022-09-19 22:04:14 +0300

    seminar

---
+++ Ex_001/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+78 -> 8 
+12-> 2 
+85 -> 8
+*/
+
+int x = new Random().Next(10, 99);
+Console.WriteLine(x);
+
+int res_x = (x % 10);
+int res_y = (x / 10);
+
+if (res_x > res_y)
+{
+    Console.WriteLine(res_x);
+}
+else
+{
+    Console.WriteLine(res_y);
+}
+
+
+
+
+
+
---
+++ Ex_004/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+16. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет*/
+
+Console.Write("Ведите первое число:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x ==  y * y || y == x * x)
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
